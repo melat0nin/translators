a0eae2e7b44b0da90be38ca65338f193c0e2f624
melat0nin <laurence.diver@gmail.com>
2020-05-21 18:57:04 +0100

    Tweaks, testing, related items bootstrap

--- Roam JSON.js
+++ Roam JSON.js
@@ -176,11 +176,18 @@ function getMetadata(item) {
             "string": "URL:: [" + item.url + "](" + item.url + ")"
         });
     }
-    if (item.tags) {
+    if (item.tags[0]) {
         metadata.children.push({
             "string": "Tags:: " + item.tags.map(o => "#[[" + o.tag + "]]").join(", ")
         });
     }
+    if (typeof item.relations === "object") {
+        // TODO: get metadata of related items (just URLs for now)
+        metadata.children.push({
+             "string": "Related:: " + Object.values(item.relations)[0].join(", ")
+        });
+    }
+
     return metadata;
 }
 
@@ -207,15 +214,23 @@ function getNotes(item) {
     return notes;
 }
 
+function getRelated(item) {
+
+}
+
 function doExport() {
+
+    /* Forget collections for now
+    var collections = [];
+    while (collection = Zotero.nextCollection()) {
+        collections.push(collection);
+    }
+    Z.write(ZU.varDump(collections));
+    */
 
     var item, exportData = [];
     while (item = Zotero.nextItem()) {
-
-        //Z.debug(ZU.varDump(item.getCollections()));
-
-
-
+        Z.write(ZU.varDump(item));
         var roamItem = {}, itemChildren = [];
         roamItem.title = item.title;
         var metadata = getMetadata(item); // Get item metadata
@@ -225,48 +240,8 @@ function doExport() {
             itemChildren.push(notes);
         }
         roamItem.children = itemChildren;
-        //roamItem["edit-time"] = Date.parse(item.dateModified)/1000;
+        roamItem["edit-time"] = Date.parse(ZU.strToISO(item.dateModified))/1000;
         exportData.push(roamItem);
     }
     Zotero.write(JSON.stringify(exportData, null, "\t"));
-
-
-
-
-    /*
-
-        articles = getArticles();
-
-
-
-        // Add articles to their respective collection(s)
-        for (var key in articles) {
-            if (!articles.hasOwnProperty(key)) continue; // skip loop if the property is from prototype
-            var article = articles[key];
-            var artCollections = article.collections;
-            for (var i = 0; i < artCollections.length; i++) { // Loop collection keys this article is in
-                var collectionKey = artCollections[i]; // Current collection key
-                if (typeof collections[collectionKey] != 'undefined' && collections[collectionKey] instanceof Object) {
-                    collections[collectionKey].articles[key] = article;
-                }
-            }
-        }
-
-        // Nest child collections
-        var nestedCollections = new Array();
-        for (var key in collections) {
-            if (!collections.hasOwnProperty(key)) continue; // skip loop if the property is from prototype
-            var collection = collections[key];
-            if (collectionKeys.indexOf(collection.parentKey) > -1) { // If a collection's parent isn't the root
-                collections[collection.parentKey].collections[key] = collection; // Nest this collection in its parent
-                nestedCollections.push(key);
-            }
-        }
-        for (var i = 0; i < nestedCollections.length; i++) { // Delete nested collections from root level (they've been nested)
-            delete collections[nestedCollections[i]];
-        }
-
-        // Output
-        Z.write(JSON.stringify(collections, null, 4));
-    */
 }
